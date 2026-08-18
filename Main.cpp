14e909f5b88e9e0b54117f4900b1bcc5ba860cb1
Santiago <Santiago@PODEROSA>
2026-08-17 22:50:03 -0500

    Agregar archivos de proyecto.

---
+++ Main.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"  FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+int main() 
+{
+	//Inicializar GLFW
+	glfwInit();
+
+	//Configurar GLFW, decirle que versión de OpenGL queremos usar
+	//En este caso, la versión 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//Decirle a GLFW que queremos usar el perfil core
+	//Esto significa que no queremos usar funciones obsoletas de OpenGL
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+	};
+
+
+
+
+	//Crear una ventana de 800x600 llamada Aprender openGL
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Aprender OpenGL", NULL, NULL);
+	//Verificar que la ventana se haya creado correctamente
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	//Decirle a GLFW que la ventana que acabamos de crear es el contexto actual 
+	glfwMakeContextCurrent(window);
+	//Cargar todas las funciones de OpenGL con GLAD
+	gladLoadGL();
+
+
+	//Decirle a OpenGL el tamaño de la ventana
+	//Esto es importante para que OpenGL sepa cómo renderizar la escena
+	glViewport(0, 0, 800, 600);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+
+
+	GLuint VAO,VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+	//Decirle a OpenGL el color de fondo de la ventana
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	//Limpiar el buffer de color
+	glClear(GL_COLOR_BUFFER_BIT);
+	//Intercambiar el buffer de color
+	glfwSwapBuffers(window);
+
+
+	//Bucle principal de la aplicación
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+
+		//Limpiar el buffer de color
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	//Eliminar la ventana y terminar GLFW
+	glfwDestroyWindow(window);
+	//Terminar GLFW
+	glfwTerminate();
+	return 0;
+}
